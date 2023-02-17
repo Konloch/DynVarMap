faadc91a04bc346c1662a1fae80d09538aa30d8e
Konloch <konloch@gmail.com>
2023-02-17 05:11:03 -0700

    Update DynVarMap.java

--- src/main/java/com/konloch/dynvarmap/DynVarMap.java
+++ src/main/java/com/konloch/dynvarmap/DynVarMap.java
@@ -874,6 +874,6 @@ public class DynVarMap
      */
     public static void main(String[] args)
     {
-        throw new RuntimeException("Incorrect usage");
+	    throw new RuntimeException("Incorrect usage - for information on how to use this correctly visit https://konloch.com/DynVarMap/");
     }
 }
